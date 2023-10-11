6d193ef2d56b5eca9e28aac44422cfd63fad735b
Luca Suizer Daniel <lucasuizerdaniel@gmail.com>
2023-10-11 17:45:57 -0300

    fix<DAOs> Added connection and save functions to DAOWaiterSQL Added functions for connect and save a waiter to the interface DAOWaiterSQL.
    Added by Luca Suizer Daniel

--- Restobar/src/restobar/Persistence/DAOWaiterSQL.java
+++ Restobar/src/restobar/Persistence/DAOWaiterSQL.java
@@ -1,14 +1,61 @@
 package restobar.Persistence;
 
 import java.util.List;
+import java.sql.*;
 import restobar.DTOs.DTOWaiter;
 
 public class DAOWaiterSQL implements DAOInterface<DTOWaiter>
 {
-
+    private Connection connect() throws SQLException
+    {
+        return DriverManager.getConnection("jdbc:mariadb://localhost:3306/waiters?user=root&password=pass");
+    }
     @Override
-    public void save(DTOWaiter t) throws DAOException {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void save(DTOWaiter t) throws DAOException
+    {
+        Connection con=null;
+        PreparedStatement stmt=null;
+        ResultSet res=null;
+        
+        String sql="INSERT INTO waiters(name,lastName) VALUES(?,?);";
+        try
+        {
+            con=connect();
+            stmt=con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1,t.getName());
+            stmt.setString(2,t.getLastName());
+            stmt.executeUpdate();
+            res=stmt.getGeneratedKeys();
+            if(res.next())
+                t.setId(res.getInt(1));
+        }catch(SQLException ex)
+        {
+            throw new DAOException(ex.getMessage());
+        }finally
+        {
+            if(res != null)
+            {
+                try
+                {
+                    res.close();
+                    res = null;
+                }catch (SQLException ex)
+                {
+                    throw new DAOException(ex.getMessage());
+                }
+            }
+            if(stmt != null)
+            {
+                try
+                {
+                    stmt.close();
+                    stmt = null;
+                }catch (SQLException ex)
+                {
+                    throw new DAOException(ex.getMessage());
+                }
+            }
+        }
     }
 
     @Override
